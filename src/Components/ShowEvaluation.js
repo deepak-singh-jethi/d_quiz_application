64de9c3cf0ba5ace883bd3a61ab4c3336a811fc2
deepak-singh-jethi <deepujethi@yahoo.com>
2024-02-04 17:23:01 +0530

    change in style of table

--- src/Components/ShowEvaluation.js
+++ src/Components/ShowEvaluation.js
@@ -10,43 +10,49 @@ function ShowEvaluation() {
   };
 
   return (
-    <div className="min-h-screen max-w-fit mx-4 sm:mx-10">
+    <div className="min-h-screen max-w-fit mx-4 sm:mx-10 eval">
       <div
         className="w-full overflow-x-auto overflow-y-scroll"
-        style={{ maxHeight: "500px" }}>
+        style={{ maxHeight: "600px" }}>
         <div className="relative">
           <table className="min-w-full bg-white border border-gray-300 ">
             <thead className="sticky top-0 bg-white z-100">
               <tr className="hidden sm:table-row">
-                <th className="py-2 px-2 sm:px-4 border-b">Question</th>
-                <th className="py-2 px-2 sm:px-4 border-b">Correct Answer</th>
-                <th className="py-2 px-2 sm:px-4 border-b">Selected Answer</th>
-                <th className="py-2 px-2 sm:px-4 border-b">Result</th>
+                <th className="py-2 px-2 sm:px-4 border-2">No. </th>
+                <th className="py-2 px-2 sm:px-4 border-2">Question</th>
+                <th className="py-2 px-2 sm:px-4 border-2">Correct Answer</th>
+                <th className="py-2 px-2 sm:px-4 border-2">Selected Answer</th>
+                <th className="py-2 px-2 sm:px-4 border-2">Result</th>
               </tr>
             </thead>
             <tbody className="max-h-400 overflow-y-scroll text-stone-500">
               {qusAns.map((qusAns, index) => (
                 <tr key={qusAns.question} className="hover:bg-gray-100">
-                  <td className="sm:table-cell py-2 px-2 sm:px-4 border text-stone-800 text-center">
-                    {index}
+                  <td className="sm:table-cell py-2 px-2 sm:px-4 border-2 text-stone-800 text-center">
+                    {index + 1}
                   </td>
                   <td
-                    className="sm:table-cell py-2 px-2 sm:px-4 border"
+                    className="sm:table-cell py-2 px-2 sm:px-4 border-2"
                     dangerouslySetInnerHTML={renderHTML(qusAns.question)}
                   />
                   <td
-                    className="sm:table-cell py-2 px-2 sm:px-4 border"
+                    className="sm:table-cell py-2 px-2 sm:px-4 border-2"
                     dangerouslySetInnerHTML={renderHTML(qusAns.correct_answer)}
                   />
                   <td
-                    className="sm:table-cell py-2 px-2 sm:px-4 border"
+                    className="sm:table-cell py-2 px-2 sm:px-4 border-2"
                     dangerouslySetInnerHTML={renderHTML(qusAns.selectedAns)}
                   />
-                  <td className="sm:table-cell py-2 px-2 sm:px-4 border">
+                  <td
+                    className={`sm:table-cell py-2 px-2 sm:px-4 border-2 text-center ${
+                      qusAns.selectedAns === qusAns.correct_answer
+                        ? "bg-green-300"
+                        : "bg-red-300"
+                    }`}>
                     {qusAns.selectedAns === qusAns.correct_answer ? (
-                      <span className="bg-green-300 p-2 sm:p-3">1</span>
+                      <span className=" p-2 sm:p-3">1</span>
                     ) : (
-                      <span className="bg-red-300 p-2 sm:p-3">0</span>
+                      <span className=" p-2 sm:p-3">0</span>
                     )}
                   </td>
                 </tr>
