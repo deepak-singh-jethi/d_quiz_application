a9f7aaea301c1ac70f073bb27c77969332e67e1b
deepak-singh-jethi <deepujethi@yahoo.com>
2024-02-04 17:34:33 +0530

    change button position in resultPage

--- src/Components/ResultPage.js
+++ src/Components/ResultPage.js
@@ -18,10 +18,12 @@ function ResultPage({ setDisplayPage, setShowEvaluation }) {
     });
   }
   return (
-    <main className="border-2 p-12 flex flex-col w-[300px] items-center  gap-8 bg-sky-700 text-stone-100 m-3 mt-10 rounded-2xl">
-      <h1 className="text-stone-50 font-extrabold	text-3xl">Result!! 🫣</h1>
-      <p>Your score is {score} </p>
-      <div className="flex gap-4">
+    <>
+      <main className="border-2 p-12 flex flex-col w-[300px] items-center  gap-8 bg-sky-700 text-stone-100 m-3 mt-10 rounded-2xl">
+        <h1 className="text-stone-50 font-extrabold	text-3xl">Result!! 🫣</h1>
+        <p>Your score is {score} </p>
+      </main>
+      <div className="flex gap-4 m-10">
         <button
           className="bg-sky-300 px-4 py-2 rounded-2xl hover:bg-blue-400 hover:text-stone-50"
           onClick={handleClick}>
@@ -33,7 +35,7 @@ function ResultPage({ setDisplayPage, setShowEvaluation }) {
           Evaluation
         </button>
       </div>
-    </main>
+    </>
   );
 }
 
