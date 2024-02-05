3948a0a72c5a717c802651d5bc3dc4d88736fd5f
deepak-singh-jethi <deepujethi@yahoo.com>
2024-02-05 13:55:29 +0530

    final commit of 5 feb

--- src/Components/CloseConfirm.js
+++ src/Components/CloseConfirm.js
@@ -2,8 +2,10 @@ import React from "react";
 const btnStyle = "px-3 py-2 bg-stone-200  mt-4 rounded-xl";
 function CloseConfirm({ onConfirm, onCancel }) {
   return (
-    <div className="bg-sky-500 px-12 py-10 flex flex-col gap-6 rounded-xl">
-      <h1>Are you sure to Submit this Test?</h1>
+    <div className="bg-sky-600 px-12 py-10 flex flex-col gap-6 rounded-xl">
+      <h1 className="text-red-200 font-extrabold text-xl">
+        Are you sure to Submit this Test?
+      </h1>
       <button onClick={onCancel} className={`${btnStyle} hover:bg-green-400`}>
         No
       </button>
